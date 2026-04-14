047b21ccad68efc7aebd5b1a1f5d856643f47a2f
Mohamed Lamine Bamba <bamomolam@gmail.com>
2026-04-14 16:45:13 +0000

    CRUD : Create and read new chirps

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $chirps = Chirp::with('user')
+            ->latest()
+            ->take(50)
+            ->get();
+
+        return view('home', compact('chirps'));
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $validated = $request->validate(
+            [
+                'message' => 'required|string|max:255'
+            ],
+            [
+                'message.required' => 'Please write something to chirp!',
+                'message.max' => 'Chirps must be 255 characters or less.',
+
+            ]
+        );
+
+        //create the chirps
+        Chirp::create([
+            'user_id' => null,
+            'message' => $validated['message']
+        ]);
+
+        //redirect back to the feed
+        return redirect(route('home'))->with('success', 'Chirp created !');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
